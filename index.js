9f86f4afd5462c238a7dea1ff59f69a63e2c326f
Aditya Bal Gupta <guptaaditya70993@gmail.com>
2022-12-11 19:37:28 +0530

    delete added

--- index.js
+++ index.js
@@ -23,18 +23,26 @@ function showNotes() {
   } else {
     notes = JSON.parse(notes);
   }
-  let html = '';
+  let html = "";
   notes.forEach((element, index) => {
     html += `
     <div class="card mx-2 my-2" style="width: 18rem;">
         <div class="card-body">
-            <h5 class="card-title">Note ${index+1}</h5>
+            <h5 class="card-title">Note ${index + 1}</h5>
             <p class="card-text">${element}</p>
-            <a href="#" class="btn btn-primary deleteButton">Delete</a>
+            <button id = "${index}" onClick = "deleteNote(this.id)" class="btn btn-primary deleteButton">Delete</button>
         </div>
     </div>`;
   });
-  let yourNotes = document.querySelector('#yourNotes');
-  if(notes.length != 0)
-    yourNotes.innerHTML = html;
+  let yourNotes = document.querySelector("#yourNotes");
+  yourNotes.innerHTML = html;
+}
+
+function deleteNote(index) {
+  let notes = localStorage.getItem("notes");
+  notesObj = JSON.parse(notes);
+  notesObj.splice(index, 1);
+  console.log(notesObj);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  showNotes();
 }
